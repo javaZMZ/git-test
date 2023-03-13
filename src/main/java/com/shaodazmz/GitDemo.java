49e1c60047f3ef727538fd0b835dcad24d999c01
zmz <1592353995@qq.com>
2023-03-13 09:59:46 +0800

    first commit

---
+++ src/main/java/com/shaodazmz/GitDemo.java
@@ -0,0 +1,13 @@
+package com.shaodazmz;
+
+/**
+ * @Classname GitDemo
+ * @Description TODO
+ * @Date 2023/3/13 9:56
+ * @Created by shaodazmz
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("zmzniubi");
+    }
+}
