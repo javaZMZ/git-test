3896172513b1750df9a23ae7e067a2621693bcfe
zmz <1592353995@qq.com>
2023-03-13 10:02:06 +0800

    3th commit

--- src/main/java/com/shaodazmz/GitDemo.java
+++ src/main/java/com/shaodazmz/GitDemo.java
@@ -9,8 +9,8 @@ package com.shaodazmz;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("zmzniubi");
-        System.out.println("zmzniubi");
-        System.out.println("dhyabsyhc");
+        System.out.println("hello git 3");
+
 
 
     }
